cc65d7f2ea6ceef36bdb58c6da0c88f458a78589
Amit Prajapati <243986054+Amit-ram-Prajapati@users.noreply.github.com>
2026-01-05 18:15:30 +0530

    Create 11_positive_neg_zero.py

---
+++ 11_positive_neg_zero.py
@@ -0,0 +1,9 @@
+
+# user inpt neg pos zero
+value = int(input("Enter number : "))
+if(value > 0):
+  print(value, "is a positive")
+elif(value < 0):
+  print(value, "is negative")
+else:
+  print(value, "is zero")
